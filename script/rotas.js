10dd8c0a7e1d37cb8ce5262b046b7173f3e1c797
ithlima <ithalolima25.05@gmail.com>
2025-10-12 11:15:58 -0300

    update rotas.js

--- script/rotas.js
+++ script/rotas.js
@@ -1,3 +1,21 @@
-function abrirsb(){
-    window.location.href = "./view/saude_pag.html";
+function rotas() {
+    const rotasMapa = {
+        "san_bas": "../view/saneamento_pag.html",
+        "saude": "../view/saude_pag.html",
+        "seguranca": "../view/seguranca_pag.html",
+        "educacao": "../view/educacao_pag.html"
+    };
+
+    for (let id in rotasMapa) {
+        const quadrado = document.getElementById(id);
+
+        if (quadrado) {
+            quadrado.addEventListener('click', function(event) {
+                event.preventDefault();
+                window.location.href = rotasMapa[id];
+            });
+        }
+    }
 }
+
+rotas();
