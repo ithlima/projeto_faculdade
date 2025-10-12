ae67fcf44f38db35a76b6786175a8af28a60515d
ithalo <133586542+ithlima@users.noreply.github.com>
2025-10-12 11:35:11 -0300

    Atualizar o rotas.js

--- script/rotas.js
+++ script/rotas.js
@@ -1,9 +1,9 @@
 function rotas() {
     const rotasMapa = {
-        "san_bas": "../view/saneamento_pag.html",
-        "saude": "../view/saude_pag.html",
-        "seguranca": "../view/seguranca_pag.html",
-        "educacao": "../view/educacao_pag.html"
+        "san_bas": "view/saneamento_pag.html",
+        "saude": "view/saude_pag.html",
+        "seguranca": "view/seguranca_pag.html",
+        "educacao": "view/educacao_pag.html"
     };
 
     for (let id in rotasMapa) {
